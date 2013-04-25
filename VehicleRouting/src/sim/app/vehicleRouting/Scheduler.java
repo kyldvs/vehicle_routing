4c1dc5f69b273d1f6a8ca5b5cf299f266112e6dd
Daniel Hull <dhull06@gmail.com>
2013-04-25 03:30:55 -0400

    stuff on stuff

--- VehicleRouting/src/sim/app/vehicleRouting/Scheduler.java
+++
@@ -1,9 +0,0 @@
-package sim.app.vehicleRouting;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.PriorityQueue;
-
-public interface Scheduler {
-	public void run(List<Vehicle> vehicles, List<Job> unassignedJobs, HashMap<Vehicle, PriorityQueue<Job>> assignments);
-}
